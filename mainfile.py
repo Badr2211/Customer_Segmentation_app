eabaf61f8476593e5632e955a574e405b004a5b8
Mohamed Badr <115509225+Badr2211@users.noreply.github.com>
2024-01-23 04:51:13 +0200

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -31,7 +31,7 @@ def uniq (df):
     return df
 
 def load():
-    df = pd.read_excel('OnlineRetail.xlsx',engine='openpyxl')
+    df = pd.read_excel('OnlineRetail.xlsx')
     return df
 
 """- Load the data"""
